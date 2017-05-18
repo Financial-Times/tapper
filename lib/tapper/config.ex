92f6a6f2f337c0794b55b2aa9c7cca48eaf6c45b
Ellis Pritchard <ellis.pritchard@ft.com>
2017-05-18 15:39:07 +0100

    support erlang modules name for reporter config (atoms starting with lower case)

--- lib/tapper/config.ex
+++ lib/tapper/config.ex
@@ -15,12 +15,13 @@ defmodule Tapper.Config do
   def to_ip(val) when is_tuple(val) and tuple_size(val) === 4, do: val
   def to_ip(val) when is_tuple(val) and tuple_size(val) === 8, do: val
   def to_ip(val) when is_binary(val) do
-    {:ok, ip} = :inet_parse.address(String.to_charlist(val))
+    {:ok, ip} = :inet.parse_address(String.to_charlist(val))
     ip
   end
 
   def to_atom(val) when is_atom(val), do: val
   def to_atom(val = "Elixir." <> _rest) when is_binary(val), do: String.to_atom(val)
+  def to_atom(val = <<c::utf8, _rest::binary>>) when is_binary(val) and c >= ?a and c <= ?z, do: String.to_atom(val)
   def to_atom(val) when is_binary(val), do: String.to_atom("Elixir." <> val)
 
 end
