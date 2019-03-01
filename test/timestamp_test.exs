45145d7649a5cd287398b3fe70686c63d285a55e
Indrek Juhkam <indrek@urgas.eu>
2019-03-01 13:25:20 +0200

    Fix deprecation warnings from System.convert_time_unit/3
    Plural from_unit and to_unit are deprecated in newer elixir versions.
    
    This caused:
    ```
      warning: deprecated time unit: :milliseconds. A time unit should be :second, :millisecond, :microsecond, :nanosecond, or a positive integer
    ```

--- test/timestamp_test.exs
+++ test/timestamp_test.exs
@@ -14,20 +14,20 @@ defmodule TimestampTest do
   test "to_absolute/1 adjusts instant to offset and microseconds" do
     ts = {timestamp, offset} = Timestamp.instant()
 
-    assert Timestamp.to_absolute(ts) == System.convert_time_unit(timestamp + offset, :native, :microseconds)
+    assert Timestamp.to_absolute(ts) == System.convert_time_unit(timestamp + offset, :native, :microsecond)
   end
 
   test "duration/2 calculates difference in microseconds" do
     t1 = {timestamp, _offset} = Timestamp.instant()
     t2 = {timestamp + 5_000, 100_000_000}
 
-    assert Timestamp.duration(t1, t2) === System.convert_time_unit(5_000, :native, :microseconds)
+    assert Timestamp.duration(t1, t2) === System.convert_time_unit(5_000, :native, :microsecond)
   end
 
   test "incr/3 adds in native units" do
     t = {timestamp, offset} = Timestamp.instant()
-    assert Timestamp.incr(t, 50, :milliseconds) == {
-      timestamp + System.convert_time_unit(50, :milliseconds, :native), offset
+    assert Timestamp.incr(t, 50, :millisecond) == {
+      timestamp + System.convert_time_unit(50, :millisecond, :native), offset
     }
   end
 
