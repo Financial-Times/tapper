9a2f2ec08a573e3078ddea4e3eac089b0980ff24
Ellis Pritchard <ellis.pritchard@ft.com>
2019-03-22 21:02:36 +0000

    parse optimization attempt 1 (failed)

--- lib/tapper/ids.ex
+++ lib/tapper/ids.ex
@@ -110,7 +110,7 @@ defmodule Tapper.Id do
 
 end
 
-defmodule Tapper.Id.Utils do
+defmodule Tapper.Id.HexConversion do
   @moduledoc false
 
   @doc "Lower-case base-16 conversion"
@@ -130,31 +130,19 @@ defmodule Tapper.TraceId do
   @moduledoc """
   Generate, parse or format a top-level trace id.
 
-  The TraceId comprises the 128-bit Zipkin id encoded as a lower-case hex string.
+  The TraceId comprises 128-bits encoded as a lower-case hex string.
   """
   @type t :: binary()
 
   defstruct [:value]   # NB only used as wrapper for e.g. logging format
 
-  import Bitwise, only: [<<<: 2]
-  require Tapper.Id.Utils
-  Tapper.Id.Utils.gen_inline_hex()
+  require Tapper.Id.HexConversion
+  Tapper.Id.HexConversion.gen_inline_hex()
 
   @doc "generate a trace id"
   @spec generate() :: t
-  def generate(), do: generate(:inline_hex)
-
-  def generate(:'0_4_0') do
-    <<id :: size(128)>> = :crypto.strong_rand_bytes(16)
-    {id, uniq()}
-  end
-
-  def generate(:base_mod) do
-    Base.encode16(:crypto.strong_rand_bytes(16), case: :lower)
-  end
-
-  def generate(:inline_hex) do
-    <<c1, c2, c3, c4, c5, c6, c7, c8, c9 ,c10, c11, c12, c13, c14, c15, c16>> = :crypto.strong_rand_bytes(16)
+  def generate() do
+    <<c1, c2, c3, c4, c5, c6, c7, c8, c9 ,c10, c11, c12, c13, c14, c15, c16, _ :: bits>> = :crypto.strong_rand_bytes(16)
     <<
       hex(c1)::bytes-size(2),
       hex(c2)::bytes-size(2),
@@ -175,70 +163,6 @@ defmodule Tapper.TraceId do
   >>
   end
 
-  def generate(:inline_hex_no_size) do
-    <<c1, c2, c3, c4, c5, c6, c7, c8, c9 ,c10, c11, c12, c13, c14, c15, c16>> = :crypto.strong_rand_bytes(16)
-    <<
-      hex(c1)::bytes,
-      hex(c2)::bytes,
-      hex(c3)::bytes,
-      hex(c4)::bytes,
-      hex(c5)::bytes,
-      hex(c6)::bytes,
-      hex(c7)::bytes,
-      hex(c8)::bytes,
-      hex(c9)::bytes,
-      hex(c10)::bytes,
-      hex(c11)::bytes,
-      hex(c12)::bytes,
-      hex(c13)::bytes,
-      hex(c14)::bytes,
-      hex(c15)::bytes,
-      hex(c16)::bytes
-    >>
-  end
-
-
-  def generate(:inline_hex_bl) do
-    l = :binary.bin_to_list(:crypto.strong_rand_bytes(16))
-    :binary.list_to_bin(:lists.map(&hex/1, l))
-  end
-
-  def generate(:lookup_table) do
-    <<c1, c2, c3, c4, c5, c6, c7, c8, c9 ,c10, c11, c12, c13, c14, c15, c16>> = :crypto.strong_rand_bytes(16)
-    # t = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff"
-    t = lookup_table()
-    <<
-      :binary.part(t, c1 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c2 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c3 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c4 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c5 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c6 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c7 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c8 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c9 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c10 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c11 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c12 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c13 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c14 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c15 <<< 1, 2)::binary-size(2),
-      :binary.part(t, c16 <<< 1, 2)::binary-size(2)
-    >>
-  end
-
-  def generate(:lookup_table_comprehension) do
-    t = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff"
-    # t = lookup_table()
-    for <<c <- :crypto.strong_rand_bytes(16)>>, into: <<>>, do: :binary.part(t, c <<< 1, 2)
-  end
-
-  l = for i <- 0..255, into: <<>> do
-    Base.encode16(<<i::8>>, case: :lower)
-  end
-  defp lookup_table(), do: unquote(l)
-  defp uniq(), do: System.unique_integer([:monotonic, :positive])
-
   @doc "format a trace id for logs etc."
   @spec format(trace_id :: t) :: String.t
   def format(trace_id)
@@ -251,22 +175,26 @@ defmodule Tapper.TraceId do
   def to_hex(trace_id), do: trace_id
 
   @doc "parse a trace id from a hex string, for propagation etc."
-  @spec parse(String.t) :: {:ok, t} | :error
   def parse(<<trace_id::bytes-size(32)>>) do
-    case Integer.parse(trace_id, 16) do
-      :error -> :error
-      {_, remaining} when byte_size(remaining) == 0 -> {:ok, trace_id}
-      _ -> :error
+    if not_valid_lower_case_hex?(trace_id) do
+      :error
+    else
+      {:ok, trace_id}
     end
   end
   def parse(<<trace_id::bytes-size(16)>>) do
-    case Integer.parse(trace_id, 16) do
-      :error -> :error
-      {_, remaining} when byte_size(remaining) == 0 -> {:ok, trace_id}
-      _ -> :error
+    if not_valid_lower_case_hex?(trace_id) do
+      :error
+    else
+      {:ok, trace_id}
     end
   end
   def parse(_), do: :error
+
+  defp not_valid_lower_case_hex?(b) do
+    # we care not for Unicode here, so hit the metal...
+    :lists.any(fn x -> (x < ?0 or x > ?9) and (x < ?a or x > ?f) end, :binary.bin_to_list(b))
+  end
 
   defimpl Inspect do
     import Inspect.Algebra
@@ -289,26 +217,26 @@ defmodule Tapper.SpanId do
   @moduledoc """
   Generate, format or parse a span id.
 
-  A span id is a 64-bit integer encoded as a lower-case hex string.
+  The SpanId comprises 64-bits encoded as a lower-case hex string.
   """
   @type t :: binary()
 
-  require Tapper.Id.Utils
-  Tapper.Id.Utils.gen_inline_hex()
+  require Tapper.Id.HexConversion
+  Tapper.Id.HexConversion.gen_inline_hex()
 
   @doc "generate a span id"
   @spec generate() :: t
   def generate() do
     <<c1, c2, c3, c4, c5, c6, c7, c8>> = :crypto.strong_rand_bytes(8)
     <<
-      hex(c1)::bytes,
-      hex(c2)::bytes,
-      hex(c3)::bytes,
-      hex(c4)::bytes,
-      hex(c5)::bytes,
-      hex(c6)::bytes,
-      hex(c7)::bytes,
-      hex(c8)::bytes,
+      hex(c1)::bytes-size(2),
+      hex(c2)::bytes-size(2),
+      hex(c3)::bytes-size(2),
+      hex(c4)::bytes-size(2),
+      hex(c5)::bytes-size(2),
+      hex(c6)::bytes-size(2),
+      hex(c7)::bytes-size(2),
+      hex(c8)::bytes-size(2),
     >>
   end
 
@@ -326,12 +254,17 @@ defmodule Tapper.SpanId do
 
   @doc "parse a span id from a hex string, for propagation etc."
   @spec parse(String.t) :: {:ok, t} | :error
-  def parse(<<span_id::bytes-size(16)>>) do
-    case Integer.parse(span_id, 16) do
-      :error -> :error
-      {_, remaining} when byte_size(remaining) == 0 -> {:ok, span_id}
-      _ -> :error
+  def parse(<<trace_id::bytes-size(16)>>) do
+    if not_valid_lower_case_hex?(trace_id) do
+      :error
+    else
+      {:ok, trace_id}
     end
   end
   def parse(_), do: :error
+
+  defp not_valid_lower_case_hex?(b) do
+    # we care not for Unicode here, so hit the metal...
+    :lists.any(fn x -> (x < ?0 or x > ?9) and (x < ?a or x > ?f) end, :binary.bin_to_list(b))
+  end
 end
